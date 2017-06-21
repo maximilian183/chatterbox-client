bdbcfd7c7a96216f19cf43f512545e05067ae7a8
masato <masato@prosperworks.com>
2017-06-20 21:21:43 -0700

    spec runner in progress

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,75 @@
 // YOUR CODE HERE:
+// Hello Masato
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+var app = {
+
+  server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+
+  init: function(){
+    return
+  },
+  send: function(message){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+        console.log(data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function(){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'GET',
+      data: JSON,
+      contentType: 'application/json',
+      success: function (data) {
+        // console.table(data.results);
+        console.log(data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  clearMessages: function(){
+    $('#chats').html('');
+  },
+  renderMessage: function(message){
+    var element = `<div>
+                    <span>${message.username}</span>
+                    <span>${message.text}</span>
+                    <span>${message.roomname}</span>
+                  </div>`;
+    $('#chats').append(element);
+    $('#main').append(`<button onclick="app.handleUsernameClick()" class="username">${message.username}</button>`);
+  },
+  renderRoom: function(roomname){
+    var element = `<div>
+                    <span>${roomname}</span>
+                  </div>`;
+    $('#roomSelect').append(element);
+  },
+  handleUsernameClick: function(){
+    return
+  },
+  handleSubmit: function(){
+    console.log('triggered handleSubmit')
+  }
+}
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -120,6 +120,7 @@ describe('chatterbox', function() {
         app.init();
 
         $('#send .submit').trigger('submit');
+        console.log($('#send .submit'))
         expect(app.handleSubmit.calledOnce).to.be.true;
 
         app.handleSubmit.restore();
