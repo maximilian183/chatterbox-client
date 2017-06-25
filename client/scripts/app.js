2e8917f53adc6db0d0a99df71a711acbd45acdca
maximilian183 <max.kwok@gmail.com>
2017-06-24 17:09:39 -0700

    Chatterbox with Friends, DM, and Room Create done!

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,17 @@
-// YOUR CODE HERE:
-// Hello Masato
-
-// var message = {
-//   username: 'shawndrost',
-//   text: 'trololo',
-//   roomname: '4chan'
-// };
-
 $(document).ready(function() {
+
   $('#send .submit').on('submit', function(e) {
     app.handleSubmit();
   });
 
+  $('#send .dmSubmit').on('submit', function(e) {
+    alert('dmSubmit');
+    app.sendDM(window.dmUsername, true);
+  });
+
   app.init();
 
-  $('#roomSelect select').on('change', function(e) {
+  $('#roomSelect').on('change', function(e) {
     if (this.value === 'Pick a room') {
       app.clearMessages();
     } else {
@@ -27,18 +24,29 @@ $(document).ready(function() {
 window.data = null;
 window.currentRoomname = null;
 window.username = window.location.search.replace('?username=', '');
+window.dmUsername = null;
 
 var app = {
   server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages?limit=1000&order=-createdAt',
   init: function() {
     // fetch data from server
-    app.fetch();
+
+    // set setInterval to fetch function / execute per 10sec
+
+    setInterval(function(){app.fetch()},1000);
+
   },
   processing: function() {
+
     //Get all the Room names in drop down
     var roomnamesObject = {};
     roomnamesObject.roomnameSort = [];
+
+    var friends = {};
+
     for (let obj of window.data.results) {
+
+      //Creating Roomlist
       var key = String(obj.roomname).trim();
       if (roomnamesObject.hasOwnProperty(key)) {
         roomnamesObject[key] = roomnamesObject[key] + 1;
@@ -46,11 +54,39 @@ var app = {
         roomnamesObject[key] = 1;
         roomnamesObject.roomnameSort.push(key);
       }
+
+      //Creating Friendlist
+      // check if type of JSON.parse() obj is true
+      if ( obj.roomname !== null && obj.roomname !== undefined ){
+        if ( obj.roomname[0] === '{'){
+         // if yes, add roomname into friends object
+          var friend = JSON.parse(obj.roomname);
+          friends[friend.to] = true;
+        }
+      }
     }
+
     roomnamesObject.roomnameSort = roomnamesObject.roomnameSort.sort();
+    //Need to reset room dropdown
+    $('#roomSelect').html('');
     for (let i of roomnamesObject.roomnameSort) {
       app.renderRoom(i);
     }
+
+    $("#roomSelect").val(window.currentRoomname);
+
+    //Need to reset friends list
+    $('#list').html('');
+    for (let i of Object.keys(friends)) {
+      app.renderFriends(i);
+    }
+
+      // if roomname is already selected
+        // render messages to selected room
+    if (window.currentRoomname !== null) {
+      app.renderRoomMessages(window.currentRoomname);
+    }
+
   },
   send: function(message) {
     $.ajax({
@@ -89,10 +125,10 @@ var app = {
   clearMessages: function() {
     $('#chats').html('');
   },
-  renderMessage: function(message, who, newMessage) {
-    var element = `<div class="chat ${who}">
-                    <a href=# onclick="app.handleUsernameClick()" class="username ${who}">${message.username}</a>
-                    <span class="talk-bubble round ${who}">${message.text}</span>
+  renderMessage: function(message, cssStyleforMe, newMessage) {
+    var element = `<div class="chat ${cssStyleforMe}">
+                    <a href=# onclick="app.handleUsernameClick('${message.username}')" class="username ${cssStyleforMe}">${message.username}</a>
+                    <span class="talk-bubble round ${cssStyleforMe}">${message.text}</span>
                   </div>`;
     if (newMessage !== undefined) {
       $('#chats').prepend(element);
@@ -100,18 +136,46 @@ var app = {
       $('#chats').append(element);
     }
   },
+  renderFriends: function(friend) {
+    var element = `<a href=# onclick="app.handleFriends('${friend}')" class="username">${friend}</a><br>`;
+    $('#list').append(element);
+  },
+  createNewRoom: function() {
+    // display popup window to take new chatroom name
+    var newRoomname = prompt('Name your new room');
+
+    if (newRoomname !== '') {
+    // construct message with:
+      // username: me
+      // roonname: entered text
+      // text: empty
+      var message = {
+        username : window.username,
+        roomname : newRoomname,
+        text : ''
+      }
+    // use app.send method and pass the argument above
+      app.send(message);
+    // set #roomSelect & global var roomname to entered name
+      window.currentRoomname = newRoomname;
+    // render new chatroom messages
+      app.renderRoomMessages(newRoomname);
+    }
+  },
   renderRoom: function(roomname) {
-    var element = `<option value="${roomname}">
-                    ${roomname}
-                  </option>`;
-    $('#roomSelect select').append(element);
+    if ( roomname[0] !== '{') {
+      var element = `<option value="${roomname}">
+                      ${roomname}
+                    </option>`;
+      $('#roomSelect').append(element);
+    }
   },
   renderRoomMessages: function(roomname) {
     var count = 0;
     app.clearMessages();
     for (let obj of window.data.results) {
       if ( obj.roomname === roomname && obj.text !== undefined) {
-        if ( obj.username !== undefined && app.NOT_TROLL(obj)) {
+        if ( obj.username !== undefined && app.NOT_TROLL(obj) && obj.text.length > 0) {
           if (obj.username === window.username) {
             app.renderMessage(obj);
             count++;
@@ -135,9 +199,24 @@ var app = {
     }
     return true;
   },
-  handleUsernameClick: function() {
-    console.log('triggered handleUsernameClick');
+  handleUsernameClick: function(username) {
+    window.dmUsername = username;
+    $('#to').val(username);
     return;
+  },
+  handleFriends: function(username){
+    app.handleUsernameClick(username);
+
+    // stringify to/friend
+    var directMessage = {
+      to: username,
+      friend: true
+    }
+
+    // clear out displayed messages
+    // pass it as argument into renderRoomMessages()
+    app.renderRoomMessages(JSON.stringify(directMessage));
+    window.currentRoomname = JSON.stringify(directMessage);
   },
   handleSubmit: function() {
     console.log('triggered handleSubmit');
@@ -159,7 +238,22 @@ var app = {
 
     app.send(message);
     app.renderMessage(message, 'me', 'newMessage');
+  },
+  sendDM: function(to, FRIEND){
+    if ( $('#dm').val() === '' ) {
+      alert('What do you want to message?');
+      return false;
+    }
+
+    var directMessage = {
+      to: to,
+      friend: FRIEND
+    }
+    var message = {
+      username: window.username,
+      text: $('#dm').val(),
+      roomname: JSON.stringify(directMessage)
+    };
+    app.send(message);
   }
 };
-
-//
